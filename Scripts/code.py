76765d00194bbd5aee6835eb7fdf47f554ea6fd6
aaasssdddggg <50709121+aaasssdddggg@users.noreply.github.com>
2022-02-08 16:57:01 -0500

    Add files via upload

---
+++ Scripts/code.py
@@ -0,0 +1,8 @@
+
+import datetime
+import time
+
+while(True):
+    time.sleep(2)  # 5 seconds.
+    # to print the id of the records added in the collection(for debugging, this value can be observed in the heroku logs)
+    print("hi.. this is a test")
