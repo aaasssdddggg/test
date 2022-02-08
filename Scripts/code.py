a6e5caa4846616f378b998fcdfe420609a039349
aaasssdddggg <50709121+aaasssdddggg@users.noreply.github.com>
2022-02-08 17:59:12 -0500

    Update code.py

--- Scripts/code.py
+++ Scripts/code.py
@@ -1,8 +1,30 @@
 
+
 import datetime
 import time
+import smtplib
+
 
 while(True):
     time.sleep(2)  # 5 seconds.
+    # creates SMTP session
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+
+    # start TLS for security
+    s.starttls()
+
+    # Authentication
+    s.login("testgoodenough@gmail.com", "testgoodenough@123")
+
+    # message to be sent
+    message = "this is a test222"
+
+    # sending the mail
+    s.sendmail("sender_email_id", "xpxpfarah@gmail.com", message)
+
+    # terminating the session
+    s.quit()
+
     # to print the id of the records added in the collection(for debugging, this value can be observed in the heroku logs)
-    print("hi.. this is a test")
+    print("changed.. this is a test")
+
