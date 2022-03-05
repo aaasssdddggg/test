8160e15c71fcdc2d37a5ea0e40c19cf156766565
aaasssdddggg <50709121+aaasssdddggg@users.noreply.github.com>
2022-03-04 22:54:38 -0500

    Update code.py

--- Scripts/code.py
+++ Scripts/code.py
@@ -25,4 +25,4 @@ while(True):
     # s.quit()
 
     # to print the id of the records added in the collection(for debugging, this value can be observed in the heroku logs)
-    print("now changed.. this is a test")
+    print("now changed.. this is a test 2")
